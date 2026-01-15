9e6e8327e9f4fd07df7cebf01de55e7bf6ebeb50
GerardoGarciaSCG <20253ds066@utez.edu.mx>
2026-01-15 08:55:55 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,7 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Semana 2");
-        System.out.println("Versión base");
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("2 + 2 = 4"));
+    IO.println(String.format("5 * 3 = 15    "));
     }
-}
+
