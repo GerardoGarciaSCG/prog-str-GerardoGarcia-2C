9e886c98f124cd0576d890cfa81d012c5cfde765
GerardoGarciaSCG <20253ds066@utez.edu.mx>
2026-01-15 08:58:15 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -3,7 +3,10 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Semana 2"));
+    IO.println(String.format("ersión base"));
+
     IO.println(String.format("2 + 2 = 4"));
-    IO.println(String.format("5 * 3 = 15    "));
+    IO.println(String.format("5 * 3 = 15 "));
     }
 
